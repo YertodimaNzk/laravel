4ff19743c7bd2b387a90c02bfff9072feeb4dbc2
Yeremia Sutanto <92780200+YertodimaNzk@users.noreply.github.com>
2023-02-05 15:24:26 +0700

    Update login.blade.php

--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
-
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
 @section('main')
-
+      <form class="form">
+        <div class="form-control">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email">
+        </div>
+        <br>
+        <div class="form-control">
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password">
+        </div>
+        <br>
+        <input type="submit" value="Submit" class="submit-button">
+      </form>
+      <p class="register-text">
+        Don't have an account? <a href="/register">Click here</a> to register.
+      </p>
 @endsection
