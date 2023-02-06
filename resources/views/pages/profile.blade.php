1f59917d1ed7c228ca87ee517b5147d128199bc9
Yeremia Sutanto <92780200+YertodimaNzk@users.noreply.github.com>
2023-02-06 22:16:01 +0700

    Change PHP echo

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,17 +7,17 @@
 
 <div class="profile-container">
   <div class="profile-picture">
-    <img src="<?php echo $profile_picture; ?>" alt="Profile Picture">
+    <img src="{{$profile_picture; }}" alt="Profile Picture">
     <input type="file" class="browse-picture" name="profile_picture">
   </div>
   <div class="profile- details">
-    <p>First Name: <input type="text" name="first_ name" value="<?php echo $first_name; ?>"></p>
-    <p>Last Name: <input type="text" name="last_ name" value="<?php echo $last_name; ?>"></p>
-    <p>Email: <input type="email" name="email" value="<?php echo $email; ?>"></p>
-    <p>Role: <input type="text" name="role" value="<?php echo $role; ?>"></p>
+    <p>First Name: <input type="text" name="first_ name" value="{{$first_name; }}"></p>
+    <p>Last Name: <input type="text" name="last_ name" value="{{$last_name; }}"></p>
+    <p>Email: <input type="email" name="email" value="{{$email; }}"></p>
+    <p>Role: <input type="text" name="role" value="{{$role; }}"></p>
     <p>Gender:
-      <input type="radio" name="gender" value="male" <?php echo $gender == 'male' ? 'checked' : ''; ?>> Male
-      <input type="radio" name="gender" value="female" <?php echo $gender == 'female' ? 'checked' : ''; ?>> Female
+      <input type="radio" name="gender" value="male" {{$gender == 'male' ? 'checked' : ''; }} Male
+      <input type="radio" name="gender" value="female" {{$gender == 'female' ? 'checked' : ''; }} Female
     </p>
     <p>Password: <input type="password" name="password"></p>
     <p>Confirm Password: <input type="password" name="confirm_ password"></p>
