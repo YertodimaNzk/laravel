cd8840aac5904371eead00c794402224d1beb566
Rin Moe <rinchanmoe.moe@gmail.com>
2023-02-07 13:50:28 +0700

    Styling Nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
-<nav>
-    <ul>
+<nav class="main-nav">
+    <ul class="main-navbar">
       <li><a href="/home">Home</a></li>
       <li><a href="/cart">Cart</a></li>
       <li><a href="/profile">Profile</a></li>
-      <li><a href="/account-management">Account Management</a></li>
+      <li><a href="/account/maintenance">Account Maintenance</a></li>
     </ul>
 </nav>
